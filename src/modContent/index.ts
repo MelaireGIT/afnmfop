6eb58e98d8a6d0cca9eff34cbc648bf58035f4f9
Stefan Gutlederer <gutlederer.stefan@gmail.com>
2026-02-07 23:15:58 +0100

    Created a repository

--- src/modContent/index.ts
+++ src/modContent/index.ts
@@ -1,31 +1,233 @@
-import { TreasureItem, Technique } from 'afnm-types';
+import { PlayerSpriteImages, PlayerSprite } from 'afnm-types';
 
-import icon from '../assets/image.png';
+//importing female sprite images
+import fopFemale1base from '../assets/female/fopFemale1base.png';
+import fopFemale1agg from '../assets/female/fopFemale1agg.png';
+import fopFemale1def from '../assets/female/fopFemale1def.png';
+import fopFemale1hit from '../assets/female/fopFemale1hit.png';
+import fopFemale1off from '../assets/female/fopFemale1off.png';
+import fopFemale1sup from '../assets/female/fopFemale1sup.png';
+import fopFemale1uti from '../assets/female/fopFemale1uti.png';
 
-const myTreasure: TreasureItem = {
-  kind: 'treasure',
-  name: 'The Best Treasure',
-  description: 'Wooo mod content.',
-  icon: icon,
-  stacks: 1,
-  rarity: 'mundane',
-  realm: 'coreFormation',
-};
-window.modAPI.actions.addItem(myTreasure);
+import fopFemale2base from '../assets/female/fopFemale2base.png';
+import fopFemale2agg from '../assets/female/fopFemale2agg.png';
+import fopFemale2def from '../assets/female/fopFemale2def.png';
+import fopFemale2hit from '../assets/female/fopFemale2hit.png';
+import fopFemale2off from '../assets/female/fopFemale2off.png';
+import fopFemale2sup from '../assets/female/fopFemale2sup.png';
+import fopFemale2uti from '../assets/female/fopFemale2uti.png';
 
-const myTechnique: Technique = {
-  name: 'Test technique',
-  icon: '',
-  type: 'fist',
-  effects: [
-    {
-      kind: 'buffSelf',
-      buff: window.modAPI.gameData.techniqueBuffs.fist.flow,
-      amount: {
-        value: 1,
-        stat: undefined,
-      },
-    },
-  ],
-};
-window.modAPI.actions.addTechnique(myTechnique);
+import fopFemale3base from '../assets/female/fopFemale3base.png';
+import fopFemale3agg from '../assets/female/fopFemale3agg.png';
+import fopFemale3def from '../assets/female/fopFemale3def.png';
+import fopFemale3hit from '../assets/female/fopFemale3hit.png';
+import fopFemale3off from '../assets/female/fopFemale3off.png';
+import fopFemale3sup from '../assets/female/fopFemale3sup.png';
+import fopFemale3uti from '../assets/female/fopFemale3uti.png';
+
+import fopFemale4base from '../assets/female/fopFemale4base.png';
+import fopFemale4agg from '../assets/female/fopFemale4agg.png';
+import fopFemale4def from '../assets/female/fopFemale4def.png';
+import fopFemale4hit from '../assets/female/fopFemale4hit.png';
+import fopFemale4off from '../assets/female/fopFemale4off.png';
+import fopFemale4sup from '../assets/female/fopFemale4sup.png';
+import fopFemale4uti from '../assets/female/fopFemale4uti.png';
+
+import fopFemale5base from '../assets/female/fopFemale5base.png';
+import fopFemale5agg from '../assets/female/fopFemale5agg.png';
+import fopFemale5def from '../assets/female/fopFemale5def.png';
+import fopFemale5hit from '../assets/female/fopFemale5hit.png';
+import fopFemale5off from '../assets/female/fopFemale5off.png';
+import fopFemale5sup from '../assets/female/fopFemale5sup.png';
+import fopFemale5uti from '../assets/female/fopFemale5uti.png';
+
+//importing male sprite images
+import fopMale1base from '../assets/male/fopMale1base.png';
+import fopMale1agg from '../assets/male/fopMale1agg.png';
+import fopMale1def from '../assets/male/fopMale1def.png';
+import fopMale1hit from '../assets/male/fopMale1hit.png';
+import fopMale1off from '../assets/male/fopMale1off.png';
+import fopMale1sup from '../assets/male/fopMale1sup.png';
+import fopMale1uti from '../assets/male/fopMale1uti.png';
+
+import fopMale2base from '../assets/male/fopMale2base.png';
+import fopMale2agg from '../assets/male/fopMale2agg.png';
+import fopMale2def from '../assets/male/fopMale2def.png';
+import fopMale2hit from '../assets/male/fopMale2hit.png';
+import fopMale2off from '../assets/male/fopMale2off.png';
+import fopMale2sup from '../assets/male/fopMale2sup.png';
+import fopMale2uti from '../assets/male/fopMale2uti.png';
+
+import fopMale3base from '../assets/male/fopMale3base.png';
+import fopMale3agg from '../assets/male/fopMale3agg.png';
+import fopMale3def from '../assets/male/fopMale3def.png';
+import fopMale3hit from '../assets/male/fopMale3hit.png';
+import fopMale3off from '../assets/male/fopMale3off.png';
+import fopMale3sup from '../assets/male/fopMale3sup.png';
+import fopMale3uti from '../assets/male/fopMale3uti.png';
+
+import fopMale4base from '../assets/male/fopMale4base.png';
+import fopMale4agg from '../assets/male/fopMale4agg.png';
+import fopMale4def from '../assets/male/fopMale4def.png';
+import fopMale4hit from '../assets/male/fopMale4hit.png';
+import fopMale4off from '../assets/male/fopMale4off.png';
+import fopMale4sup from '../assets/male/fopMale4sup.png';
+import fopMale4uti from '../assets/male/fopMale4uti.png';
+
+
+import fopMale5base from '../assets/male/fopMale5base.png';
+import fopMale5agg from '../assets/male/fopMale5agg.png';
+import fopMale5def from '../assets/male/fopMale5def.png';
+import fopMale5hit from '../assets/male/fopMale5hit.png';
+import fopMale5off from '../assets/male/fopMale5off.png';
+import fopMale5sup from '../assets/male/fopMale5sup.png';
+import fopMale5uti from '../assets/male/fopMale5uti.png';
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopFemale1',
+  name: 'FOP Female 1',
+  gender: 'female',
+  sprites: {
+    base: fopFemale1base,
+    aggressive: fopFemale1agg,
+    defensive: fopFemale1def,
+    hit: fopFemale1hit,
+    offensive: fopFemale1off,
+    support: fopFemale1sup,
+    utility: fopFemale1uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopFemale2',
+  name: 'FOP Female 2',
+  gender: 'female',
+  sprites: {
+    base: fopFemale2base,
+    aggressive: fopFemale2agg,
+    defensive: fopFemale2def,
+    hit: fopFemale2hit,
+    offensive: fopFemale2off,
+    support: fopFemale2sup,
+    utility: fopFemale2uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopFemale3',
+  name: 'FOP Female 3',
+  gender: 'female',
+  sprites: {
+    base: fopFemale3base,
+    aggressive: fopFemale3agg,
+    defensive: fopFemale3def,
+    hit: fopFemale3hit,
+    offensive: fopFemale3off,
+    support: fopFemale3sup,
+    utility: fopFemale3uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopFemale4',
+  name: 'FOP Female 4',
+  gender: 'female',
+  sprites: {
+    base: fopFemale4base,
+    aggressive: fopFemale4agg,
+    defensive: fopFemale4def,
+    hit: fopFemale4hit,
+    offensive: fopFemale4off,
+    support: fopFemale4sup,
+    utility: fopFemale4uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopFemale5',
+  name: 'FOP Female 5',
+  gender: 'female',
+  sprites: {
+    base: fopFemale5base,
+    aggressive: fopFemale5agg,
+    defensive: fopFemale5def,
+    hit: fopFemale5hit,
+    offensive: fopFemale5off,
+    support: fopFemale5sup,
+    utility: fopFemale5uti,
+  }
+});
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopMale1',
+  name: 'FOP Male 1',
+  gender: 'male',
+  sprites: {
+    base: fopMale1base,
+    aggressive: fopMale1agg,
+    defensive: fopMale1def,
+    hit: fopMale1hit,
+    offensive: fopMale1off,
+    support: fopMale1sup,
+    utility: fopMale1uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopMale2',
+  name: 'FOP Male 2',
+  gender: 'male',
+  sprites: {
+    base: fopMale2base,
+    aggressive: fopMale2agg,
+    defensive: fopMale2def,
+    hit: fopMale2hit,
+    offensive: fopMale2off,
+    support: fopMale2sup,
+    utility: fopMale2uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopMale3',
+  name: 'FOP Male 3',
+  gender: 'male',
+  sprites: {
+    base: fopMale3base,
+    aggressive: fopMale3agg,
+    defensive: fopMale3def,
+    hit: fopMale3hit,
+    offensive: fopMale3off,
+    support: fopMale3sup,
+    utility: fopMale3uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopMale4',
+  name: 'FOP Male 4',
+  gender: 'male',
+  sprites: {
+    base: fopMale4base,
+    aggressive: fopMale4agg,
+    defensive: fopMale4def,
+    hit: fopMale4hit,
+    offensive: fopMale4off,
+    support: fopMale4sup,
+    utility: fopMale4uti,
+  }
+});
+
+window.modAPI.actions.addPlayerSprite({
+  id: 'fopMale5',
+  name: 'FOP Male 5',
+  gender: 'male',
+  sprites: {
+    base: fopMale5base,
+    aggressive: fopMale5agg,
+    defensive: fopMale5def,
+    hit: fopMale5hit,
+    offensive: fopMale5off,
+    support: fopMale5sup,
+    utility: fopMale5uti,
+  }
+});
